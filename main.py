cf3e07366ffe34890f11f1ff0efdc7bbf47d384f
Akriti-Ghosh <akriti.ghosh@techment.com>
2021-07-29 23:42:23 +0530

    Mask created

--- main.py
+++ main.py
@@ -2,20 +2,39 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+# Processing
+from matplotlib import pyplot as plt
+from matplotlib import gridspec as gds
 
 mp_selfie = mp.solutions.selfie_segmentation
 
-with mp_selfie.SelfieSegmentation(model_selection = 0) as model:
+cap = cv2.VideoCapture(0)
+# Create with statement for model 
+with mp_selfie.SelfieSegmentation(model_selection=0) as model: 
     while cap.isOpened():
-        _, img = cap.read()
-        cv2.imshow("Selfie", img)
+        ret, frame = cap.read()
+        
+        # Apply segmentation
+        frame.flags.writeable = False
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        res = model.process(frame)
+        frame.flags.writeable = True
 
-        key = cv2.waitKey(0) & 0xFF
-        if key == ord('q'):
+        cv2.imshow('Selfie Seg', frame)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-
+cap.release()
 cv2.destroyAllWindows()
-exit()
 
 
+plt.figure(figsize=(15,15))
+grid = gds.GridSpec(1,2) #(row, column)
+ax0 = plt.subplot(grid[0])
+ax1 = plt.subplot(grid[1])
+ax0.imshow(frame)
+ax1.imshow(res.segmentation_mask)
+plt.show()
+
+# CREATE BACKGROUND
+background = np.zeros(frame.shape, dtype=np.uint8)
